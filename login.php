2554c82b7ea51cfc3a21ef8c519bc495b18c4d9b
ncristiansa <ncristiansalinasandia@gmail.com>
2018-12-07 23:12:23 +0100

    cosas news

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
 <head>
 	<link rel="stylesheet" type="text/css" href="estiloScrum.css">
 	<title>Inicia Sesión</title>
-	<script type="text/javascript" src="script.js"></script>
+	<script type="text/javascript" src="scripts.js"></script>
 </head>
 <body>
 
@@ -18,7 +18,7 @@ echo "<h2 class='titulo'>LOGIN</h2>";
 			echo "<input type='text' name='nom'><br>";
 		echo "Password: <br>";
 			echo "<input type='password' name='password'><br>";
-		echo "<input type='submit' value='Enviar' name='submit'><br>";
+		echo "<input type='submit' value='Enviar' name='submit' id='btn'><br>";
 	echo "</form>";
 	
 	$nombre=$_POST["nom"];
@@ -33,17 +33,49 @@ echo "<h2 class='titulo'>LOGIN</h2>";
 
 	$log="mysql:host=localhost;dbname=DBPrueba";
 	$conn = new PDO($log,"Administrador","P@ssw0rd");
-	$stmt = $conn->prepare("SELECT * FROM Users WHERE nickname=:nombre and passwd=SHA2(:pass,512)");
-	$stmt->bindValue(':nombre',$nombre);
-	$stmt->bindValue(':pass',$pass);
-	$stmt->execute();
-	$result=$stmt->rowCount();
-if(isset($_POST['submit'])){
-	if($result==1){
-		header("Location: vistainicialP2.php");
-	}
-}
 
+	$queryUser = $conn->prepare("SELECT nickname FROM Users WHERE nickname=:nombre");
+	$queryUser->bindValue(':nombre',$nombre);
+	$queryUser->execute();
+	$resultUser=$queryUser->rowCount();
+
+	$queryPass = $conn->prepare("SELECT passwd FROM Users WHERE passwd=SHA2(:pass,512)");
+	$queryPass->bindValue(':pass', $pass);
+	$queryPass->execute();
+	$resultPass=$queryPass->rowCount();
+	//$stmt = $conn->prepare("SELECT * FROM Users WHERE nickname=:nombre and passwd=SHA2(:pass,512)");
+	//$stmt->bindValue(':nombre',$nombre);
+	//$stmt->bindValue(':pass',$pass);
+	//$stmt->execute();
+	//$result=$stmt->rowCount();
+
+	/*
+		Esta condición nos permite controlar si el usuario ha hecho click en el botón "Enviar".
+	*/
+	if(isset($_POST['submit'])){
+		/*
+			Comprobamos si la consulta del nombre ha devuelto una fila, si se cumple...
+		*/
+		if($resultUser==1){
+			echo "nombre usuario bien";
+		}else{
+			echo '<script type="text/javascript">addMessageError("Usuario inválido.");</script>';
+		}
+		/*
+			Comprobamos si la consulta del password ha devuelto una fila, si se cumple
+		*/
+		if($resultPass==1){
+			echo "contraseña usuario bien";
+		}else{
+			'<script type="text/javascript">addMessageError("Contraseña inválido.");</script>';
+		}
+		/*
+			Si ambas condiciones se cumplen nos enviará a la web
+		*/
+		if($resultUser==1 && $resultPass==1){
+			header("Location: vistainicialP2.php");
+		}
+	}
 
 ?>
 </body>
